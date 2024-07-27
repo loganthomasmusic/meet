8ef2183a988a014c539b4ab558efd50e69d060b5
loganthomasmusic <ltstewart95@gmail.com>
2024-07-27 16:32:59 -0400

    Implement unit testing

---
+++ src/components/Event.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const Event = ({ event }) => {
+
+    const [showDetails, setShowDetails] = useState(false);
+
+    if (!event) {
+        return null
+    }
+    return (
+        <li>
+            <div>{event.summary}</div>
+            <div>{event.created}</div>
+            <div>{event.location} </div>
+            <button onClick={() => setShowDetails(true)}>show details</button>
+            <button onClick={() => setShowDetails(false)}>hide details</button>
+            {showDetails && (
+                <div>
+                    <p>Event Details Here</p>
+                </div>
+            )}
+        </li>
+    );
+};
+
+export default Event;
